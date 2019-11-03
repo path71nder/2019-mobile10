c2d52be63b10a6756b7b2d9191069471923e8196
path71nder <sevillebayuaji@gmail.com>
2019-11-03 19:12:33 +0700

    Bab 10 : ViewModel Lanjut (HomeFragment)

--- SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/viewmodels/SharedScore.java
+++ SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/viewmodels/SharedScore.java
@@ -1,4 +1,49 @@
 package id.ac.polinema.skorbasket.viewmodels;
 
-public class SharedScore {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+public class SharedScore extends AndroidViewModel {
+
+    private MutableLiveData<Integer> scoreHome;
+    private MutableLiveData<Integer> scoreVisitor;
+    private MutableLiveData<Boolean> winner;
+
+    public SharedScore(@NonNull Application application) {
+        super(application);
+    }
+
+    public SharedScore(@NonNull Application application, MutableLiveData<Integer> scoreHome, MutableLiveData<Integer> scoreVisitor, MutableLiveData<Boolean> winner) {
+        super(application);
+        this.scoreHome = scoreHome;
+        this.scoreVisitor = scoreVisitor;
+        this.winner = winner;
+    }
+
+    public MutableLiveData<Integer> getScoreHome() {
+        return scoreHome;
+    }
+
+    public void setScoreHome(MutableLiveData<Integer> scoreHome) {
+        this.scoreHome = scoreHome;
+    }
+
+    public MutableLiveData<Integer> getScoreVisitor() {
+        return scoreVisitor;
+    }
+
+    public void setScoreVisitor(MutableLiveData<Integer> scoreVisitor) {
+        this.scoreVisitor = scoreVisitor;
+    }
+
+    public MutableLiveData<Boolean> getWinner() {
+        return winner;
+    }
+
+    public void setWinner(MutableLiveData<Boolean> winner) {
+        this.winner = winner;
+    }
 }
